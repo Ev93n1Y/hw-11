a337c7e2fb11bc55cb98983b214f0ea1e5b79941
Yevhen <96913622+Ev93n1Y@users.noreply.github.com>
2022-06-20 18:38:24 +0300

    Update Utils.java

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,1 +1,9 @@
-
+public static class Utils{
+    public static ArrayList<String> createNamesList(String names...){
+        private ArrayList<String> namesList = new ArrayList<>;
+        for(String name : names){
+            namesList.add(name);
+        }
+        return namesList;
+    }
+}
